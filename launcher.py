36ccf4f8d7a5d47b4bf2c1aa764116d389be0fd8
Hidden Kitten <30309397+HiddenKitten@users.noreply.github.com>
2024-07-31 14:04:00 -0500

    Split server launching, swap method to terminate processes, setup log files. Cleanup: remove unnecessary excess explicit calls to Path() Cleanup: Only download when attempting to run, rather than when swapping selected version. Releases: fix the code that was supposed to hide releases without the zip file necessary (versions before 0.8.5 on mac)

--- launcher.py
+++ launcher.py
@@ -6,7 +6,7 @@ from os import environ, chdir
 from itertools import count
 import zipfile
 import io
-# import time
+import datetime
 # import multiprocessing
 import stat
 import subprocess
@@ -60,7 +60,7 @@ class DownloadThread(QThread):
                 archive.extractall(self.gameDirectory)
         else:
             # save the file
-            with Path(self.gameDirectory, asset.get('name')).open('wb') as file:
+            with (self.gameDirectory/asset.get('name')).open('wb') as file:
                 file.write(response.content)
 
 class launcher(QMainWindow):
@@ -82,14 +82,17 @@ class launcher(QMainWindow):
         self.updateComboBox()
         self.writeReleaseNotes()
         self.releaseSelected = self.ui.releasesComboBox.currentText()
-        self.ui.releasesComboBox.currentIndexChanged.connect(self.releaseChanged)
-        self.setGameDirectory()
+        self.gameDirectory = GAME_DIRECTORY/self.releaseSelected
+        self.ui.releasesComboBox.currentTextChanged.connect(self.releaseChanged)
         self.ui.pushButton_hostServer.clicked.connect(self.hostServer)
         self.ui.pushButton_connect.clicked.connect(self.connectToServer)
         self.ui.pushButton_startAll.clicked.connect(self.startAll)
         self.ui.prereleasesCheckBox.stateChanged.connect(self.updateComboBox)
         self.download_thread = None
         self.offset = None
+        self.subprocesses = []
+        self.server_processes = {'uberdog': None, 'astron': None, 'AI': None}
+        self.done_pre_run = False
 
 
     # events to allow dragging of window
@@ -112,24 +115,19 @@ class launcher(QMainWindow):
 
     # on exit we should close any existing subprocesses
     def closeEvent(self, event):
-        if platform == 'win32':
-            subprocess.run('taskkill /f /im astrond.exe')
-            subprocess.run('taskkill /f /im launch.exe')
-        else:
-            if platform_module.machine() == 'arm64':
-                subprocess.run(['pkill', 'astrond-arm'])
-                subprocess.run(['pkill', 'launch'])
-            else:
-                subprocess.run(['pkill', 'astrond'])
-                subprocess.run(['pkill', 'launch'])
-            
+        for i in self.subprocesses:
+            if i.poll() is None:
+                i.kill()
+        for i in self.server_processes.values():
+            if i is not None and i.poll() is None:
+                i.kill()
+        super().closeEvent(event)
 
-        
-        
+
 
     def loadPrefs(self):
         try:
-            with Path(GAME_DIRECTORY, 'prefs.json').open('r', encoding='utf-8') as file:
+            with (GAME_DIRECTORY/'prefs.json').open('r', encoding='utf-8') as file:
                 prefs = json.load(file)
         except OSError:
             print("preferences could not be loaded.\nIf this is the first time running, this can be ignored.")
@@ -152,7 +150,7 @@ class launcher(QMainWindow):
         if self.prefs.get('remember'):
             self.prefs['username'] = self.ui.lineEdit_username.text()
             self.prefs['serverIP'] = self.ui.lineEdit_ipAddress.text()
-        with Path(GAME_DIRECTORY, 'prefs.json').open('w', encoding='utf-8') as file:
+        with (GAME_DIRECTORY/'prefs.json').open('w', encoding='utf-8') as file:
             json.dump(self.prefs, file)
 
     def updateComboBox(self):
@@ -166,31 +164,62 @@ class launcher(QMainWindow):
         self.ui.releasesComboBox.setCurrentIndex(0)
         self.releaseSelected = self.ui.releasesComboBox.currentText()
 
+
+    def pre_run(self):
+        #if we've already done this since the last time the version was changed, there's no need.
+        if self.done_pre_run:
+            return
+        # ensure we've downloaded the selected release.
+        self.downloadRelease()
+        if self.download_thread is not None:
+            self.download_thread.wait()
+        # macos folder structure didn't match windows, at least up to v0.10.4, ensure compatibility with those versions.
+        if (self.gameDirectory/'release').exists():
+            for file in self.gameDirectory.glob('release/*'):
+                file.rename(self.gameDirectory/file.name)
+            (self.gameDirectory/'release').rmdir()
+        # ensure launch has execute permissions for platforms other than windows.
+        if platform != 'win32':
+            modes = (self.gameDirectory/'game'/'launch').stat().st_mode
+            if not modes & stat.S_IXUSR:
+                (self.gameDirectory/'game'/'launch').chmod(modes | stat.S_IXUSR)
+            # do the same for astrond
+            for file in (self.gameDirectory/'game'/'astron').glob('*'):
+                modes = file.stat().st_mode
+                if not modes & stat.S_IXUSR:
+                    file.chmod(modes | stat.S_IXUSR)
+        # ensure the log directory exists.
+        (self.gameDirectory/'log').mkdir(exist_ok=True)
+        # TODO: setup cleaning log files out, delete older than 24 hours, probably.
+        self.done_pre_run = True
+
     def startAll(self):
         self.hostServer()
         self.connectToServer()
 
     def hostServer(self):
-        # check if we downloaded the release we selected
-        self.downloadRelease()
+        # run pre run if necessary
+        self.pre_run()
         self.startServerThreads()
 
     def runClient(self):
         self.savePrefs()
         chdir(self.gameDirectory/'game')
-        if platform == 'darwin':
-            modes = Path('launch').stat().st_mode
-            if not modes & stat.S_IXUSR:
-                Path('launch').chmod(modes | stat.S_IXUSR)
-            subprocess.Popen(['./launch'])
-        elif platform == 'win32':
-            subprocess.Popen('launch', creationflags=subprocess.CREATE_NO_WINDOW)
-        elif platform.startswith('linux'):
-            # use wine
-            subprocess.Popen(['wine', 'launch'])
+        launchFile = self.gameDirectory/'game'/'launch'
+        with (self.gameDirectory/'log'/f'client-{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.log').open('w', encoding='utf-8') as logfile:
+            if platform == 'darwin':
+                p = subprocess.Popen([ launchFile ], stdout=logfile, stderr=subprocess.STDOUT)
+            elif platform == 'win32':
+                p = subprocess.Popen([ launchFile ], creationflags=subprocess.CREATE_NO_WINDOW, stdout=logfile, stderr=subprocess.STDOUT)
+            elif platform.startswith('linux'):
+                # use wine
+                p = subprocess.Popen([ 'wine', launchFile ], stdout=logfile, stderr=subprocess.STDOUT)
+        return p
 
 
     def connectToServer(self):
+        #execute pre run steps, if necessary.
+        self.pre_run()
         # read from lineEdit_ipAddress, and lineEdit_username,
         # default ip : 127.0.0.1
         # default username : player1
@@ -200,8 +229,6 @@ class launcher(QMainWindow):
             ip = "127.0.0.1"
         if not username:
             username = "player1"
-        # check if we downloaded the release we selected
-        self.downloadRelease()
         if self.download_thread:
             self.download_thread.finished.connect(lambda: self.startClientThread(username, ip))
         else:
@@ -213,88 +240,71 @@ class launcher(QMainWindow):
         environ['SERVICE_TO_RUN'] = 'CLIENT'
         environ['TTOFF_LOGIN_TOKEN'] = username
         environ['TTOFF_GAME_SERVER'] = ip
-        self.runClient()
+        self.subprocesses.append(self.runClient())
 
     def startServerThreads(self):
-        # astron
+        for service in ['astron', 'uberdog', 'AI']:
+            if (self.server_processes.get(service, None) is None # service never started,
+               or not self.server_processes[service].poll() is None): # or service has stopped.
+                self.server_processes[service] = self.startService(service)
+
+    def startAstronServer(self):
         astron_dir = self.gameDirectory / 'game' / 'astron'
-        chdir(astron_dir)
-        # print current dir of game directory
-        # first if we are on a unix system chmod the astrond files
-        if platform != 'win32':
-            # for each file in astron folder
-            for file in Path('.').glob('*'):
-                modes = Path(file).stat().st_mode
-                if not modes & stat.S_IXUSR:
-                    Path(file).chmod(modes | stat.S_IXUSR)
-
-        # macos has a different astrond for arm64
-        # check for the arm64 architecture using uname 
-        if platform_module.machine() == 'arm64':
-            astrond_executable = './astrond-arm'
-        else:
-            astrond_executable = 'astrond'
-        # Check if the config file exists and is readable
         config_path = astron_dir / 'config' / 'astrond.yml'
+        chdir(astron_dir)
         # Run the astrond subprocess
-        if platform == 'win32':
-            subprocess.Popen([astrond_executable, '--loglevel', 'info', str(config_path)], creationflags=subprocess.CREATE_NEW_CONSOLE)
+        # macos has a different astrond for arm64
+        # check for the arm64 architecture using uname
+        if platform_module.machine() == 'arm64':
+            astrond_executable = self.gameDirectory/'game'/'astron'/'astrond-arm'
         else:
-            subprocess.Popen([f'./{astrond_executable}', '--loglevel', 'info', str(config_path)])
+            astrond_executable = self.gameDirectory/'game'/'astron'/'astrond'
+        with (self.gameDirectory/'log'/f'astrond-{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.log').open('w', encoding='utf-8') as logfile:
+            if platform == 'win32':
+                p = subprocess.Popen([astrond_executable, '--loglevel', 'info', str(config_path)], creationflags=subprocess.CREATE_NO_WINDOW, stdout=logfile, stderr=subprocess.STDOUT)
+            else:
+                p = subprocess.Popen([astrond_executable, '--loglevel', 'info', str(config_path)], stdout=logfile, stderr=subprocess.STDOUT)
+        return p
 
-        # uberdog
-        # if we are on unix systems then chmod launch
-        if platform != 'win32':
-            modes = Path(self.gameDirectory / 'game' / 'launch').stat().st_mode
-            if not modes & stat.S_IXUSR:
-                Path(self.gameDirectory / 'game' / 'launch').chmod(modes | stat.S_IXUSR)
+    def startService(self, service):
         chdir(self.gameDirectory / 'game')
-        environ['SERVICE_TO_RUN'] = 'UD'
-        if platform == 'win32':
-            subprocess.Popen(['launch',
-                            '--base-channel', '1000000',
-                            '--max-channels', '999999',
-                            '--stateserver', '4002',
-                            '--astron-ip', '127.0.0.1:7199',
-                            '--eventlogger-ip', '127.0.0.1:7197',
-                            'config/common.prc',
-                            'config/production.prc'],
-                            creationflags=subprocess.CREATE_NEW_CONSOLE)
+        if service == 'astron':
+            return self.startAstronServer()
+        if service == 'uberdog':
+            environ['SERVICE_TO_RUN'] = 'UD'
+            base_channel = '1000000'
+            extra_args = []
+        elif service == 'AI':
+            environ['SERVICE_TO_RUN'] = 'AI'
+            base_channel = '401000000'
+            extra_args = ['--district-name', 'Archipelago Avenue']
         else:
-            subprocess.Popen(['./launch',
-                            '--base-channel', '1000000',
-                            '--max-channels', '999999',
-                            '--stateserver', '4002',
-                            '--astron-ip', '127.0.0.1:7199',
-                            '--eventlogger-ip', '127.0.0.1:7197',
-                            'config/common.prc',
-                            'config/production.prc'])
-
-        #AI
-        chdir(self.gameDirectory / 'game')
-        environ['SERVICE_TO_RUN'] = 'AI'
-        if platform == 'win32':
-            subprocess.Popen(['launch',
-                            '--base-channel', '401000000',
-                            '--max-channels', '999999',
-                            '--stateserver', '4002',
-                            '--astron-ip', '127.0.0.1:7199',
-                            '--eventlogger-ip', '127.0.0.1:7197',
-                            '--district-name', 'Archipelago Avenue',
-                            'config/common.prc',
-                            'config/production.prc'],
-                            creationflags=subprocess.CREATE_NEW_CONSOLE)
-        else:
-            subprocess.Popen(['./launch',
-                            '--base-channel', '401000000',
-                            '--max-channels', '999999',
-                            '--stateserver', '4002',
-                            '--astron-ip', '127.0.0.1:7199',
-                            '--eventlogger-ip', '127.0.0.1:7197',
-                            '--district-name', 'Archipelago Avenue',
-                            'config/common.prc',
-                            'config/production.prc'])
-
+            raise ValueError(f"Unknown TTAP Service: {service}")
+        with (self.gameDirectory/'log'/f'{service}-{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.log').open('w', encoding='utf-8') as logfile:
+            if platform == 'win32':
+                p = subprocess.Popen(['launch',
+                                    '--base-channel', base_channel,
+                                    '--max-channels', '999999',
+                                    '--stateserver', '4002',
+                                    '--astron-ip', '127.0.0.1:7199',
+                                    '--eventlogger-ip', '127.0.0.1:7197',
+                                    *extra_args,
+                                    'config/common.prc',
+                                    'config/production.prc'],
+                                    creationflags=subprocess.CREATE_NO_WINDOW,
+                                    stdout=logfile, stderr=subprocess.STDOUT)
+            else:
+                p = subprocess.Popen(['./launch',
+                                    '--base-channel', base_channel,
+                                    '--max-channels', '999999',
+                                    '--stateserver', '4002',
+                                    '--astron-ip', '127.0.0.1:7199',
+                                    '--eventlogger-ip', '127.0.0.1:7197',
+                                    *extra_args,
+                                    'config/common.prc',
+                                    'config/production.prc'],
+                                    stdout=logfile, stderr=subprocess.STDOUT)
+        return p
 
     # make the list of releases based on the https://github.com/toontown-archipelago/toontown-archipelago/releases API
     def getReleases(self):
@@ -305,31 +315,26 @@ class launcher(QMainWindow):
             query['page'] = i
             response = requests.get(url, query, timeout=10)
             data = response.json()
-            if not data: #github returns an empty list when you request a page further than the oldest release
+            if not data: # github returns an empty list when you request a page further than the oldest release
                 break
             for release in data:
-                releases[release.get('tag_name')] = release
                 assets = release.get('assets', [])
                 if any(item.get('name') == ZIP_NAME for item in assets):
                     releases.update({release.get('tag_name'): release})
         return releases
 
-    def releaseChanged(self):
-        self.releaseSelected = self.ui.releasesComboBox.currentText()
-        # download the release if it doesn't exist already
-        self.downloadRelease()
+    def releaseChanged(self, releaseSelected):
+        self.releaseSelected = releaseSelected
+        self.gameDirectory = GAME_DIRECTORY/releaseSelected
+        self.done_pre_run = False
 
     def writeReleaseNotes(self):
         for v in self.releases.values():
             self.ui.releaseNotesText.append(v['name'] + '\n')
             self.ui.releaseNotesText.append(v['body'])
 
-    def setGameDirectory(self):
-        self.gameDirectory = Path(GAME_DIRECTORY / self.releaseSelected)
-
     def downloadRelease(self):
-        self.setGameDirectory()
-        if Path(self.gameDirectory,'game').exists() or self.releaseSelected == "":
+        if (self.gameDirectory/'game').exists() or self.releaseSelected == "":
             return
         assets = list(
                       item for item in self.releases.get(self.releaseSelected, {}).get('assets', [])
@@ -361,11 +366,6 @@ class launcher(QMainWindow):
     def onDownloadFinished(self):
         self.ui.downloadLabel.setVisible(False)
         self.ui.installprogressBar.setVisible(False)
-        if Path(self.gameDirectory, 'release').exists():
-            for file in self.gameDirectory.glob('release/*'):
-                file.rename(self.gameDirectory/file.name)
-            Path(self.gameDirectory,'release').rmdir()
-        self.setGameDirectory()
 
 
 
