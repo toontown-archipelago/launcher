ac4b079af6c5a8e8d37ba4542fbf38e101fce75a
Hidden Kitten <30309397+HiddenKitten@users.noreply.github.com>
2024-07-30 19:10:30 -0500

    Basic Server Launch Implementation, remove unnecessary threading calls.
    subprocess.Popen runs things in a new thread already, running the client in a separate thread through threading was redundant.

--- launcher.py
+++ launcher.py
@@ -2,13 +2,14 @@
 from sys import platform, argv, exit as sys_exit
 from pathlib import Path
 from os import environ, chdir
+from itertools import count
 import zipfile
 import io
 # import time
 # import multiprocessing
 import stat
 import subprocess
-import threading
+# import threading
 import json
 import requests
 from PySide6.QtWidgets import QApplication, QMainWindow
@@ -149,7 +150,7 @@ class launcher(QMainWindow):
     def hostServer(self):
         # check if we downloaded the release we selected
         self.downloadRelease()
-        # TODO: multithreaded running of all server components including astron, uberdog and ai
+        self.startServerThreads()
 
     def runClient(self):
         self.savePrefs()
@@ -160,7 +161,7 @@ class launcher(QMainWindow):
                 Path('launch').chmod(modes | stat.S_IXUSR)
             subprocess.Popen(['./launch'])
         elif platform == 'win32':
-            subprocess.Popen('launch', creationflags=134217728)
+            subprocess.Popen('launch', creationflags=subprocess.CREATE_NO_WINDOW)
         elif platform.startswith('linux'):
             # use wine
             subprocess.Popen(['wine', 'launch'])
@@ -189,21 +190,57 @@ class launcher(QMainWindow):
         environ['SERVICE_TO_RUN'] = 'CLIENT'
         environ['TTOFF_LOGIN_TOKEN'] = username
         environ['TTOFF_GAME_SERVER'] = ip
-        clientThread = threading.Thread(target=self.runClient)
-        clientThread.start()
+        self.runClient()
+
+    def startServerThreads(self):
+        #astron
+        chdir(self.gameDirectory / 'game' / 'astron')
+        subprocess.Popen(['astrond', '--loglevel', 'info', 'config/astrond.yml'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+        #uberdog
+        chdir(self.gameDirectory / 'game')
+        environ['SERVICE_TO_RUN'] = 'UD'
+        subprocess.Popen(['launch',
+                         '--base-channel', '1000000',
+                         '--max-channels', '999999',
+                         '--stateserver', '4002',
+                         '--astron-ip', '127.0.0.1:7199',
+                         '--eventlogger-ip', '127.0.0.1:7197',
+                         'config/common.prc',
+                         'config/production.prc'],
+                         creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+        #AI
+        chdir(self.gameDirectory / 'game')
+        environ['SERVICE_TO_RUN'] = 'AI'
+        subprocess.Popen(['launch',
+                         '--base-channel', '401000000',
+                         '--max-channels', '999999',
+                         '--stateserver', '4002',
+                         '--astron-ip', '127.0.0.1:7199',
+                         '--eventlogger-ip', '127.0.0.1:7197',
+                         '--district-name', 'Archipelago Avenue',
+                         'config/common.prc',
+                         'config/production.prc'],
+                         creationflags=subprocess.CREATE_NEW_CONSOLE)
+
 
     # make the list of releases based on the https://github.com/toontown-archipelago/toontown-archipelago/releases API
     def getReleases(self):
         url = "https://api.github.com/repos/toontown-archipelago/toontown-archipelago/releases"
-        query = {'per_page': 100} #TODO: setup pagination, before we hit 100 releases.
-        response = requests.get(url, query, timeout=10)
-        data = response.json()
         releases = {}
-        for release in data:
-            releases[release.get('tag_name')] = release
-            assets = release.get('assets', [])
-            if any(item.get('name') == ZIP_NAME for item in assets):
-                releases.update({release.get('tag_name'): release})
+        query = {'per_page': 100}
+        for i in count(1):
+            query['page'] = i
+            response = requests.get(url, query, timeout=10)
+            data = response.json()
+            if not data: #github returns an empty list when you request a page further than the oldest release
+                break
+            for release in data:
+                releases[release.get('tag_name')] = release
+                assets = release.get('assets', [])
+                if any(item.get('name') == ZIP_NAME for item in assets):
+                    releases.update({release.get('tag_name'): release})
         return releases
 
     def releaseChanged(self):
